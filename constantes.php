fd31611b867dd46283677c599255db737b64263c
luluminha <luluminha@hotmail.com>
2012-11-03 17:45:04 -0200

    adicionando arquivo constantes.php

---
+++ constantes.php
@@ -0,0 +1,24 @@
+<?php
+
+define("CONSTANTE", "ola mundo", true);
+
+echo CONSTANTE;
+echo '<br>';
+echo constante;
+echo '<br>';
+echo ConStanTE;
+
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+echo __FILE__;
+echo ":";
+echo __LINE__;
+
+/*
+ * __FUNCTION__
+ * __CLASS__  (orientado a objetlo)
+ * __METHOD__   (orientado a objetlo)
+ */
+?>
